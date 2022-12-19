0ba8cd6782bba0f038e69d7f86e432414f5cc8bd
CassMelo <cassiac@yahoo.com>
2022-12-19 14:25:07 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,7 +27,7 @@ streamlit.header("Fruityvice Fruit Advice!")
 fruit_choice = streamlit.textinput('What fruit would you like to have information about?','Kiwi')
 streamlit.write('The user entered', fruit_choice)
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice 
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 # streamlit.text(fruityvice_response.json()) # writes the data to the screen
 
 # take the json version of the response and normaliza it
